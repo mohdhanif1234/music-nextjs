bb941eb04e6f3f7491b4f9ef1bdfef6e57bb8998
mohammad hanif <mohdhanif.topia@nw18.com>
2024-03-06 07:58:46 +0530

    add wavy background and animated tool tip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
@@ -8,6 +12,10 @@ export default function Home() {
       {/* <h1 className="text-2xl text-center text-white">chai aur code</h1> */}
       <HeroSection/>
       <FeaturedCourses/>
+      <WhyChooseUs/>
+      <MusicSchoolTestimonials/>
+      <UpcomingWebinars/>
+      <Instructors/>
     </main>
   );
 }
